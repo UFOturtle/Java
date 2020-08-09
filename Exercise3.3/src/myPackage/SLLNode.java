040c34848fa78a909326ba821d2771fde9f84ab5
UFOturtle <46785628+UFOturtle@users.noreply.github.com>
2020-08-08 21:00:34 -0400

    update description
    Added Linked List class, and a node class with nodes that can contain objects as data. Also added a Driver for testing

---
+++ Exercise3.3/src/myPackage/SLLNode.java
@@ -0,0 +1,16 @@
+package myPackage;
+
+public class SLLNode {
+	public Object info;
+	public SLLNode next;
+	public SLLNode () {
+		next = null;
+	}
+	public SLLNode(Object el) {
+		info = el; next = null;
+	}
+	public SLLNode(Object el, SLLNode ptr) {
+		info = el; next = ptr;
+	}
+	
+}
